5c91f07317680434b072f9ac54722c3447938df1
Anthony Williams <anthony.williams.cs@gmail.com>
2019-02-26 19:56:29 -0700

    Algorithm now passes all tests

--- Acronym/acronym.js
+++ Acronym/acronym.js
@@ -1,3 +1,33 @@
+const BANNED_WORDS = ['of', 'the', 'in', 'at', 'on', 'and', 'or', 'is', 'a', 'it']
+const WORD_DELIMITER = ' '
+
+const isSafe = word => !BANNED_WORDS.includes((word || '').toLowerCase())
+
+const capitalizeThenDeAlphabetize = word => {
+	word = (word || '').trim().replace(/it's/gi, 'its')
+
+	if (word.length < 1) {
+		return ''
+	}
+
+	const firstLetter = word[0].toUpperCase()
+	const restOfString = word.substring(1).replace(/[A-z]/gi, '')
+
+	return firstLetter + restOfString
+}
+
+const acro = inputStr => {
+	inputStr = inputStr || ''
+
+	if (inputStr.length < 1) {
+		return ''
+	}
+
+	const words = inputStr.split(WORD_DELIMITER)
+
+	return words.filter(isSafe).map(capitalizeThenDeAlphabetize).join('')
+}
+
 const tests = [
 	{
 		expected: '',
@@ -21,42 +51,27 @@ const tests = [
 	},
 ]
 
-const acro = (inputStr) => {
-	let result = ''
-	inputStr = inputStr || ''
-
-	if (inputStr.length < 1) {
-		return result
-	}
-
-	return result
-}
-
 let failures = []
-let failedTests = 0
-let passedTests = 0
-let successful = true
 
 for (const test of tests) {
 	const result = acro(test.input)
+
 	if (result === test.expected) {
-		passedTests++
 		continue
-		// console.log(`SUCCESS\n\tinput="${test.input}"\n\toutput ${result}`)
 	}
-	failedTests++
-	successful = false
 	failures.push(` FAILED\n\tinput "${test.input}"\n\toutput "${result}"\n\texpected "${test.expected}"`)
 }
 
 const decoration = '~'.repeat(10)
+console.log(`${decoration}  PASSES: ${tests.length - failures.length}  ${decoration}`)
+console.log(`${decoration} FAILURES: ${failures.length} ${decoration}`)
 
-console.log(`${decoration}  PASSES: ${passedTests}  ${decoration}`)
-
-if (!successful) {
-	console.warn(`${decoration} FAILURES: ${failedTests} ${decoration}`)
-
-	for (const failure of failures) {
-		console.warn(failure)
-	}
+for (const failure of failures) {
+	console.warn(failure)
 }
+
+if (failures.length > 0) {
+	return
+}
+
+console.log(`${decoration} ❤️ ALL PASSED! ❤️ ${decoration}`)
