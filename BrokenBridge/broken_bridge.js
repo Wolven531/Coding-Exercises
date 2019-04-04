384907dc166103442ac834c4bdcb3fe3d570a4a8
Anthony Williams <anthony.williams.cs@gmail.com>
2019-04-03 19:32:32 -0600

    Start working on broken bridge algo

--- BrokenBridge/broken_bridge.js
+++ BrokenBridge/broken_bridge.js
@@ -1,6 +1,54 @@
 const testRunner = require('../test-runner')
 
-const brokenBridge = bridge => {
+const BROKEN = 0
+const USABLE = 1
+
+const canFixHole = (lengthOfHole, sortedRemainingPlanks) => {
+	if (lengthOfHole > 1 && sortedRemainingPlanks.length === 0) {
+		return false
+	}
+	return true
+}
+
+const brokenBridge = (bridge, planks) => {
+	let sortedRemainingPlanks = planks.sort()// sort ascending (e.g. [1,2])
+	// console.log(`sortedRemainingPlanks = ${sortedRemainingPlanks}`)
+
+	let lengthOfHole = 0
+	for (let a = 0; a < bridge.length; a++) {
+		const currentSpot = bridge[a]
+		console.log(`Viewing index=${a} spot=${currentSpot} currentLengthOfHole=${lengthOfHole}`)
+		
+		// increase hole length and move forward
+		if (currentSpot === BROKEN) {
+			console.log(`Found broken, increasing hole length and moving forward...`)
+			lengthOfHole++
+			continue
+		}
+		// looking at a usable spot, and hole is cross-able, so move forward
+		if (lengthOfHole < 2) {
+			console.log(`Found usable, hole is cross-able, resetting hole length and moving forward...`)
+			lengthOfHole = 0
+			continue
+		}
+		// last hole not cross-able and no planks left
+		if (!canFixHole(lengthOfHole, sortedRemainingPlanks)) {
+			console.log(`Last hole not cross-able and no planks left, bailing...`)
+			return false
+		}
+		// // hole is 2 or more
+		// for (let b = 0; b < sortedRemainingPlanks; b++) {
+		// 	if (sortedRemainingPlanks[b] >= lengthOfHole) {
+		// 		sortedRemainingPlanks = [
+		// 			...sortedRemainingPlanks.slice(0, b),
+		// 			...sortedRemainingPlanks.slice(b + 1)
+		// 		]
+		// 		lengthOfHole = 0
+		// 		break
+		// 	}
+		// }
+	}
+
 	return true
 }
 
