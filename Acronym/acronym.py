bba4d76b1928e2a139594a95cc32d49c6815ec21
Anthony Williams <anthony.williams.cs@gmail.com>
2019-07-07 22:38:03 -0600

    Start coding exercises in Python

---
+++ Acronym/acronym.py
@@ -0,0 +1,17 @@
+'''
+    @param string input_str The string to attempt to convert to acronym
+    @returns string The result of converting the input string to an acronym
+'''
+# @staticmethod
+def acro(input_str: str):
+    return input_str
+
+# @staticmethod
+# def run_tests:
+#     assert (acro('') === ''), 'test 1 failed'
+
+# run_tests()
+
+test_one_expected = ''
+test_one_result = acro('')
+print(f'acro of "" = "{test_one_result}", expected "{test_one_expected}"')
