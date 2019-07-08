b1ed43d62237cdc686f479f0dd165bc98d9c87f6
Anthony Williams <anthony.williams.cs@gmail.com>
2019-07-07 22:48:41 -0600

    Add second test for Python Acro

--- Acronym/acronym.py
+++ Acronym/acronym.py
@@ -8,7 +8,12 @@ def acro(input_str: str):
 def run_tests():
     test_one_expected = ''
     test_one_result = acro('')
-    print(f'acro of "" = "{test_one_result}", expected "{test_one_expected}"')
+    print(f'1.) acro("") = "{test_one_result}"\t expected "{test_one_expected}"')
     assert test_one_result == test_one_expected, f'test one failed; expected "{test_one_expected}"'
+
+    test_two_expected = '.'
+    test_two_result = acro('.')
+    print(f'2.) acro(".") = "{test_two_result}"\t expected "{test_two_expected}"')
+    assert test_two_result == test_two_expected, f'test two failed; expected "{test_two_expected}"'
 
 run_tests()
