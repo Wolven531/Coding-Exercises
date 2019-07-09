58740710eb55154cb0995601d22a81f12b467cce
Anthony Williams <anthony.williams.cs@gmail.com>
2019-07-08 22:58:30 -0600

    Get test 3 passing

--- Acronym/acronym.py
+++ Acronym/acronym.py
@@ -1,9 +1,28 @@
+non_acro_words = ['and', 'is', 'the', 'a']
+symbols_to_keep = [',', '.']
+
 '''
 	@param string input_str The string to attempt to convert to acronym
 	@returns string The result of converting the input string to an acronym
 '''
-def acro(input_str: str):
-	return input_str
+def acro(input_str):
+	if (len(input_str) < 1):
+		return input_str
+
+	words = input_str.split(' ')
+	result = ''
+
+	for word in words:
+		if (len(word) < 1):
+			continue
+		if (word.lower() in non_acro_words):
+			continue
+		if (len(word) > 1):
+			result += word[0].capitalize()
+		if (word[-1] in symbols_to_keep):
+			result += word[-1]
+
+	return result
 
 '''
 	@param list<tuple(input, expected_output)> tests A list of tuples, each containing an input and expected output
@@ -14,12 +33,13 @@ def run_tests(tests):
 		test_input = test[0]
 		test_expected = test[1]
 		test_result = acro(test_input)
-		print(f'{test_num}.) acro("{test_input}") = "{test_result}"\t expected "{test_expected}"')
+		print(f'{test_num}.) acro("{test_input}") = "{test_result}"')
 		assert test_result == test_expected, f'test {test_num} failed; expected "{test_expected}"'
 
 tests = [
 	('', ''),
-	('.', '.')
+	('.', '.'),
+	('And this is how the world ends. Not with a bang, but a whimper', 'THWE.NWB,BW')
 ]
 
 run_tests(tests)
