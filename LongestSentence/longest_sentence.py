bb1f2ab1268101a07fb059c71b72402d662d87fb
Anthony Williams <anthony.williams.cs@gmail.com>
2019-07-30 23:20:32 -0600

    Add python tests for LongestSentence

--- LongestSentence/longest_sentence.py
+++ LongestSentence/longest_sentence.py
@@ -5,3 +5,23 @@ def longest_sentence(input_str):
 	words = input_str.split(' ')
 
 	return len(words)
+
+def run_tests(tests):
+	for test_num, test in enumerate(tests, start=1):
+		test_input = test[0]
+		test_expected = test[1]
+		test_result = longest_sentence(test_input)
+		print(f'{test_num}.) longest_sentence("{test_input}") = "{test_result}"')
+		assert test_result == test_expected, f'test {test_num} failed; expected "{test_expected}"'
+	num_tests = len(tests)
+	print(f'\nAll {num_tests} tests passed!\n')
+
+tests = [
+	('A test sentence. Does it work properly?', 4),
+	('A curveball..Very strange . x x', 2),
+	(' Some Test? With... Edge Cases! ', 2),
+	('', 0),
+	(' ', 0)
+]
+
+run_tests(tests)
