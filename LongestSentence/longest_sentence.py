477ea2fb45c2dab78423b8f6db2905dcd74289c5
Anthony Williams <anthony.williams.cs@gmail.com>
2019-07-31 20:08:23 -0600

    Get all tests passing for Python LongestSentence

--- LongestSentence/longest_sentence.py
+++ LongestSentence/longest_sentence.py
@@ -2,26 +2,29 @@ sentence_delims = ['.', '?', '!']
 
 def next_sentence(input_str):
 	sentence = ''
-	char_index = 0
+	remaining = ''
 
 	if len(input_str) < 1:
-		return (sentence, char_index)
+		return (sentence, remaining)
+
+	remaining_start = 0
 
 	for char in input_str:
+		remaining_start += 1
 		if char in sentence_delims:
+			remaining = input_str[remaining_start:]
 			break
 		else:
 			sentence += char
-		char_index += 1
 
 	sentence = sentence.strip()
-	print('returning sentence="' + sentence + '" and char_index=' + str(char_index))
 
-	return (sentence, char_index)
+	return (sentence, remaining)
 
 
 def longest_sentence(input_str):
 	longest = 0
+
 	if len(input_str) < 1:
 		return longest
 
@@ -29,34 +32,29 @@ def longest_sentence(input_str):
 
 	while sentence_info[0] is not '':
 		words = sentence_info[0].split(' ')
-		print('considering s=' + sentence_info[0] + ', words=' + ','.join(words))
 		sentence_length = len(words)
 
 		if sentence_length > longest:
 			longest = sentence_length
 
-		remaining = input_str[sentence_info[1] + 1:]
-
-		print('remaining="' + remaining + '"')
-
-		sentence_info = next_sentence(remaining)
+		sentence_info = next_sentence(sentence_info[1])
 
 	return longest
 
 def run_tests(tests):
+	test_count = len(tests)
 	for test_num, test in enumerate(tests, start=1):
 		test_input = test[0]
 		test_expected = test[1]
 		test_result = longest_sentence(test_input)
-		print(f'{test_num}.) longest_sentence("{test_input}") = "{test_result}"')
-		assert test_result == test_expected, f'test {test_num} failed; expected "{test_expected}"'
-	num_tests = len(tests)
-	print(f'\nAll {num_tests} tests passed!\n')
+		print(f'{test_num}.) longest_sentence("{test_input}") = {test_result}')
+		assert test_result == test_expected, f'test {test_num} failed; expected {test_expected}'
+	print(f'\nAll {test_count} tests passed!\n')
 
 tests = [
-	# ('A test sentence. Does it work properly?', 4),
-	# ('A curveball..Very strange . x x', 2),
-	# (' Some Test? With... Edge Cases! ', 2),
+	('A test sentence. Does it work properly?', 4),
+	('A curveball..Very strange . x x', 2),
+	(' Some Test? With... Edge Cases! ', 2),
 	('asdf', 1),
 	('', 0),
 	(' ', 0)
