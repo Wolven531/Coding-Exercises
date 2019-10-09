62ddae2bdac94f162169e6cb4f1749666639589a
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-08 20:58:11 -0600

    Add PrimeNumbers class with Factors function

---
+++ CSharpCodingExercises/CSharpCodingExercises/PrimeNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpCodingExercises
+{
+    public class PrimeNumbers
+    {
+        // Print all prime factors of a given number n > 1
+        public static void Factors(int n)
+        {
+            // Print each 2 that divides n 
+            while (n % 2 == 0)
+            {
+                Console.WriteLine(2 + " ");
+                n /= 2;
+            }
+
+            // n MUST be odd at this point; never compare an even (note += 2)
+            for (var i = 3; i <= Math.Sqrt(n); i += 2)
+            {
+                // While i divides n, print i and divide n 
+                while (n % i == 0)
+                {
+                    Console.WriteLine(i + " ");
+                    n /= i;
+                }
+            }
+
+            // Handle n is a prime number greater than 2 
+            if (n > 2)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
