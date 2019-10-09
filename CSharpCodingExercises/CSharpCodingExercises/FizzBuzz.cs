91416aabac7591bcd27ea7933678a741f0ed66f9
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-08 20:46:25 -0600

    Extract FizzBuzz code

---
+++ CSharpCodingExercises/CSharpCodingExercises/FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpCodingExercises
+{
+    public class FizzBuzz
+    {
+        // Write a short program that
+        //      prints each number from 1 to 100 on a new line
+        //        - For each multiple of 3, print "Fizz" instead of the number
+        //        - For each multiple of 5, print "Buzz" instead of the number
+        //        - For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number
+        public static void Execute()
+        {
+            for (var a = 1; a <= 100; a++)
+            {
+                if (a % 3 == 0 && a % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                    continue;
+                }
+                if (a % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    continue;
+                }
+                if (a % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    continue;
+                }
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
--- CSharpCodingExercises/CSharpCodingExercises/Program.cs
+++ CSharpCodingExercises/CSharpCodingExercises/Program.cs
@@ -13,35 +13,7 @@ namespace CSharpCodingExercises
             //Console.WriteLine($"The result of Fibo(4) = ${Fibonacci.FiboIter(4)}");
 
             Console.WriteLine("Running FizzBuzz...");
-            FizzBuzz();
-        }
-
-        // Write a short program that
-        //      prints each number from 1 to 100 on a new line
-        //        - For each multiple of 3, print "Fizz" instead of the number
-        //        - For each multiple of 5, print "Buzz" instead of the number
-        //        - For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number
-        private static void FizzBuzz()
-        {
-            for(var a = 1; a <= 100; a++)
-            {
-                if(a % 3 == 0 && a % 5 == 0)
-                {
-                    Console.WriteLine("FizzBuzz");
-                    continue;
-                }
-                if (a % 5 == 0)
-                {
-                    Console.WriteLine("Buzz");
-                    continue;
-                }
-                if (a % 3 == 0)
-                {
-                    Console.WriteLine("Fizz");
-                    continue;
-                }
-                Console.WriteLine(a);
-            }
+            FizzBuzz.Execute();
         }
     }
 }
